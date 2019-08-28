aed32ddcb287ab1760f966a071d89fdca800c445
StoneStoneStoneWang <yuanxingfu1314@163.com>
2019-08-28 17:42:06 +0800

    ZReq 、ZBase

---
+++ Framework/ZBase/ZBase.framework/Headers/ZBaseMix.h
@@ -0,0 +1,32 @@
+//
+//  ZBaseMix.h
+//  ZBase
+//
+//  Created by three stone 王 on 2019/8/28.
+//  Copyright © 2019 three stone 王. All rights reserved.
+//
+
+
+#if DEBUG
+
+#else
+
+#define configNaviItem OO8DEZQy9
+
+#define configOwnProperties cgtMvV7Aa
+
+#define addOwnSubViews bJzwgcs4
+
+#define configOwnSubViews RnQic6IfQM
+
+#define configLoading GJQUF9u0E
+
+#define configViewModel wqrMBuy29
+
+#define prepareData PQF2DtZbj
+
+#define configAuto cJfU6DP2zs
+
+#define addOwnSubViewController de0peN2N2F
+
+#endif
---
+++ Framework/ZReq/ZReq.framework/Headers/ZReqMix.h
@@ -0,0 +1,19 @@
+//
+//  ZReqMix.h
+//  ZReq
+//
+//  Created by three stone 王 on 2019/8/28.
+//  Copyright © 2019 three stone 王. All rights reserved.
+//
+
+#if DEBUG
+
+#else
+
+#define postWithUrl oQ739RG
+
+#define ZReqManager Z6HMEbfHL
+
+#define analysisSomeThing gS77Dezl9
+
+#endif
