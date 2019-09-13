36642d7d88e6a28b98ca9b3960d43ae769c43045
StoneStoneStoneWang <yuanxingfu1314@163.com>
2019-09-13 17:28:23 +0800

    ZCommentBridge

--- Code/ZBridge/Comment/Bridge/ZCommentBridge.swift
+++ Code/ZBridge/Comment/Bridge/ZCommentBridge.swift
@@ -7,3 +7,164 @@
 //
 
 import Foundation
+import ZTable
+import RxDataSources
+import ZCocoa
+import ZBean
+import ZHud
+
+@objc (ZCommentBridge)
+public final class ZCommentBridge: ZBaseBridge {
+    
+    typealias Section = ZAnimationSetionModel<ZCommentBean>
+    
+    var dataSource: RxTableViewSectionedAnimatedDataSource<Section>!
+    
+    var viewModel: ZCommentViewModel!
+    
+    weak var vc: ZTableLoadingViewController!
+}
+extension ZCommentBridge {
+    
+    @objc public func createTList(_ vc: ZTableLoadingViewController,encode: String) {
+        
+        self.vc = vc
+        
+        let input = ZCommentViewModel.WLInput(modelSelect: vc.tableView.rx.modelSelected(ZCommentBean.self),
+                                              itemSelect: vc.tableView.rx.itemSelected,
+                                              headerRefresh: vc.tableView.mj_header.rx.refreshing.asDriver(),
+                                              footerRefresh: vc.tableView.mj_footer.rx.refreshing.asDriver(),
+                                              encoded: encode)
+        
+        viewModel = ZCommentViewModel(input, disposed: disposed)
+        
+        let dataSource = RxTableViewSectionedAnimatedDataSource<Section>(
+            animationConfiguration: AnimationConfiguration(insertAnimation: .fade, reloadAnimation: .fade, deleteAnimation: .left),
+            decideViewTransition: { _,_,_  in return .reload },
+            configureCell: { ds, tv, ip, item in return vc.configTableViewCell(item, for: ip)})
+        
+        viewModel
+            .output
+            .tableData
+            .asDriver()
+            .map({ $0.map({ Section(header: $0.encoded, items: [$0]) }) })
+            .drive(vc.tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposed)
+        
+        let endHeaderRefreshing = viewModel.output.endHeaderRefreshing
+        
+        endHeaderRefreshing
+            .map({ _ in return true })
+            .drive(vc.tableView.mj_header.rx.endRefreshing)
+            .disposed(by: disposed)
+        
+        endHeaderRefreshing
+            .drive(onNext: { (res) in
+                switch res {
+                case .fetchList:
+                    vc.loadingStatus = .succ
+                case let .failed(msg):
+                    ZHudUtil.showInfo(msg)
+                    vc.loadingStatus = .fail
+                    
+                case .empty:
+                    vc.loadingStatus = .succ
+                    
+                    vc.tableViewEmptyShow()
+                    
+                default:
+                    break
+                }
+            })
+            .disposed(by: disposed)
+        
+        let endFooterRefreshing = viewModel.output.endFooterRefreshing
+        
+        endFooterRefreshing
+            .map({ _ in return true })
+            .drive(vc.tableView.mj_footer.rx.endRefreshing)
+            .disposed(by: disposed)
+        
+        self.dataSource = dataSource
+        
+        viewModel
+            .output
+            .zip
+            .subscribe(onNext: { (type,ip) in
+                
+                vc.tableViewSelectData(type, for: ip)
+                
+            })
+            .disposed(by: disposed)
+        
+        viewModel
+            .output
+            .footerHidden
+            .bind(to: vc.tableView.mj_footer.rx.isHidden)
+            .disposed(by: disposed)
+        
+        vc
+            .tableView
+            .rx
+            .setDelegate(self)
+            .disposed(by: disposed)
+    }
+    
+    @objc public func addComment(_ encoded: String,content: String ,succ: @escaping () -> () ) {
+        
+        ZHudUtil.show(withStatus: "发表评论中....")
+        
+        ZCommentViewModel
+            .addComment(encoded, content: content)
+            .drive(onNext: { [unowned self] (result) in
+                
+                ZHudUtil.pop()
+                
+                switch result {
+                case .operation(let comment):
+                    
+                    var value = self.viewModel.output.tableData.value
+                    
+                    if value.last!.type == .empty {
+                        
+                        value.removeLast()
+                        
+                        value.insert(comment as! ZCommentBean, at: 2)
+                        
+                        self.viewModel.output.tableData.accept(value)
+                        
+                    } else if value.last!.type == .failed {
+                        
+                        self.vc.tableView.mj_header.beginRefreshing()
+                        
+                    } else {
+                        
+                        value.insert(comment as! ZCommentBean, at: 2)
+                        
+                        self.viewModel.output.tableData.accept(value)
+                    }
+                    
+                    self.vc.tableView.scrollsToTop = true
+                    
+                    ZHudUtil.showInfo("发表评论成功!")
+                    
+                case .failed(let msg):
+                    
+                    ZHudUtil.showInfo(msg)
+                default:
+                    break
+                }
+            })
+            .disposed(by: disposed)
+    }
+}
+
+extension ZCommentBridge: UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        guard let datasource = dataSource else { return 0}
+        
+        return vc.caculate(forCell: datasource[indexPath], for: indexPath)
+    }
+}
--- Code/ZBridge/Comment/VM/ZCommentViewModel.swift
+++ Code/ZBridge/Comment/VM/ZCommentViewModel.swift
@@ -7,3 +7,228 @@
 //
 
 import Foundation
+import WLBaseViewModel
+import RxCocoa
+import RxSwift
+import WLReqKit
+import ZBean
+import WLBaseResult
+import ZRealReq
+import ZApi
+
+struct ZCommentViewModel: WLBaseViewModel {
+    
+    var input: WLInput
+    
+    var output: WLOutput
+    
+    struct WLInput {
+        
+        let modelSelect: ControlEvent<ZCommentBean>
+        
+        let itemSelect: ControlEvent<IndexPath>
+        
+        let headerRefresh: Driver<Void>
+        
+        let footerRefresh: Driver<Void>
+        
+        let page: BehaviorRelay<Int> = BehaviorRelay<Int>(value: 1)
+        
+        let encoded: String
+    }
+    
+    struct WLOutput {
+        
+        let zip: Observable<(ZCommentBean,IndexPath)>
+        
+        let tableData: BehaviorRelay<[ZCommentBean]> = BehaviorRelay<[ZCommentBean]>(value: [])
+        
+        let endHeaderRefreshing: Driver<WLBaseResult>
+        
+        let endFooterRefreshing: Driver<WLBaseResult>
+        
+        let footerHidden: BehaviorRelay<Bool> = BehaviorRelay<Bool>(value: true)
+    }
+    init(_ input: WLInput ,disposed: DisposeBag) {
+        
+        self.input = input
+        
+        let zip = Observable.zip(input.modelSelect,input.itemSelect)
+        
+        let headerRefreshData = input
+            .headerRefresh
+            .startWith(())
+            .flatMapLatest({_ in
+                
+                return onUserArrayResp(ZUserApi.fetchComments(1, targetEncoded: input.encoded))
+                    .mapArray(type: ZCommentBean.self)
+                    .map({ return $0.count > 0 ? WLBaseResult.fetchList($0) : WLBaseResult.empty })
+                    .asDriver(onErrorRecover: { return Driver.just(WLBaseResult.failed(($0 as! WLBaseError).description.0)) })
+            })
+        
+        let endHeaderRefreshing = headerRefreshData.map { $0 }
+        
+        let footerRefreshData = input
+            .footerRefresh
+            .flatMapLatest({_ in
+                
+                return onUserArrayResp(ZUserApi.fetchComments(input.page.value, targetEncoded: input.encoded))
+                    .mapArray(type: ZCommentBean.self)
+                    .map({ return $0.count > 0 ? WLBaseResult.fetchList($0) : WLBaseResult.empty })
+                    .asDriver(onErrorRecover: { return Driver.just(WLBaseResult.failed(($0 as! WLBaseError).description.0)) })
+            })
+        
+        let endFooterRefreshing = footerRefreshData.map { $0 }
+        
+        let output = WLOutput(zip: zip, endHeaderRefreshing: endHeaderRefreshing, endFooterRefreshing: endFooterRefreshing)
+        
+        headerRefreshData
+            .drive(onNext: { (result) in
+                
+                let rectangle = ZCommentBean()
+                
+                rectangle.type = .rectangle
+                
+                rectangle.content = ""
+                
+                rectangle.encoded = NSUUID().uuidString
+                
+                rectangle.identity = NSUUID().uuidString
+                
+                let total = ZCommentBean()
+                
+                total.type = .total
+                
+                total.content = "全部评论"
+                
+                total.encoded = NSUUID().uuidString
+                
+                total.identity = NSUUID().uuidString
+                
+                switch result {
+                case let .fetchList(items):
+                    
+                    input.page.accept(2)
+                    
+                    if items.count < 20 {
+                        
+                        output.footerHidden.accept(true)
+                        
+                        input.page.accept(1)
+                    } else {
+                        
+                        output.footerHidden.accept(false)
+                    }
+                    
+                    output.tableData.accept([rectangle] + [total] + items as! [ZCommentBean])
+                    
+                    let noMore = ZCommentBean()
+                    
+                    noMore.encoded = NSUUID().uuidString
+                    
+                    noMore.identity = NSUUID().uuidString
+                    
+                    noMore.type = .noMore
+                    
+                    noMore.content = "没有更多了"
+                    
+                    output.tableData.accept(output.tableData.value + [noMore])
+                    
+                case .empty:
+                    
+                    let empty = ZCommentBean()
+                    
+                    empty.encoded = NSUUID().uuidString
+                    
+                    empty.identity = NSUUID().uuidString
+                    
+                    empty.type = .empty
+                    
+                    empty.content = "暂无评论"
+                    
+                    output.tableData.accept([rectangle,total,empty])
+                case .failed:
+                    
+                    let failed = ZCommentBean()
+                    
+                    failed.type = .failed
+                    
+                    failed.identity = NSUUID().uuidString
+                    
+                    failed.encoded = NSUUID().uuidString
+                    
+                    output.tableData.accept([rectangle,total,failed])
+                default: break
+                }
+            })
+            .disposed(by: disposed)
+        
+        footerRefreshData
+            .drive(onNext: { (result) in
+                
+                switch result {
+                case let .fetchList(items):
+                    
+                    var page = input.page.value
+                    
+                    page += 1
+                    
+                    input.page.accept(page)
+                    
+                    if items.count < 20 {
+                        
+                        output.footerHidden.accept(true)
+                        
+                    } else {
+                        
+                        output.footerHidden.accept(false)
+                    }
+                    
+                    var value = output.tableData.value
+                    
+                    value += (items as! [ZCommentBean])
+                    
+                    output.tableData.accept( value)
+                    
+                    let noMore = ZCommentBean()
+                    
+                    noMore.encoded = NSUUID().uuidString
+                    
+                    noMore.identity = NSUUID().uuidString
+                    
+                    noMore.type = .noMore
+                    
+                    noMore.content = "暂无评论"
+                    
+                    output.tableData.accept(output.tableData.value + [noMore])
+                    
+                case .failed:
+                    
+                    let failed = ZCommentBean()
+                    
+                    failed.encoded = NSUUID().uuidString
+                    
+                    failed.identity = NSUUID().uuidString
+                    
+                    failed.type = .failed
+                    
+                    failed.content = "网络错误 点击重新加载"
+                    
+                    output.tableData.accept(output.tableData.value + [failed])
+                    
+                default: break
+                }
+            })
+            .disposed(by: disposed)
+        
+        self.output = output
+    }
+    
+    static func addComment(_ encoded: String,content: String) -> Driver<WLBaseResult> {
+        
+        return onUserDictResp(ZUserApi.addComment(encoded, content: content, tablename: "CircleFriends", type: "0"))
+            .mapObject(type: ZCommentBean.self)
+            .map({ WLBaseResult.operation($0) })
+            .asDriver(onErrorRecover: { return Driver.just(WLBaseResult.failed(($0 as! WLBaseError).description.0)) })
+    }
+}
